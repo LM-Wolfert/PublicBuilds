e9c39c4d46b2c4a784d413c6e9937462870f8d4e
BuildTools <unconfigured@null.spigotmc.org>
2021-03-26 18:30:27 +0000

    Combined all player related methods and classes into a single User class.

---
+++ src/me/elgamer/publicbuilds/utils/User.java
@@ -0,0 +1,64 @@
+package me.elgamer.publicbuilds.utils;
+
+import org.bukkit.entity.Player;
+
+import me.elgamer.publicbuilds.mysql.PlayerData;
+
+public class User {
+
+	public Player player;
+	public String uuid;
+	public String name;
+	public int tutorialStage;
+	public Plots plots;
+	public Accept accept;
+	public int reviewing = 0;
+	public String reasonType;
+	public int currentPlot = 0;
+	
+	public User(Player player) {
+		
+		//Set player, uuid and name variable.
+		this.player = player;
+		uuid = player.getUniqueId().toString();
+		name = player.getName();
+		
+		//Update player data.
+		updatePlayerData();
+			
+		//Continue the tutorial from where they last were.
+		tutorialStage = PlayerData.getTutorialStage(uuid);
+		Tutorial.continueTutorial(this);
+		
+		//Create plots map.
+		plots = new Plots();
+		
+	}
+	
+	//Update playerdata or create a new instance if it's their first time joining the server.
+	private void updatePlayerData() {
+		
+		if (PlayerData.playerExists(uuid)) {
+			
+			//If true then update their last online time and username.
+			PlayerData.updateTime(player.getUniqueId().toString());
+			PlayerData.updatePlayerName(player.getUniqueId().toString(), player.getName());
+			
+		} else {
+			
+			PlayerData.createPlayerInstance(player.getUniqueId().toString(), player.getName());
+			
+		}
+		
+	}
+	
+	//Update tutorial stage.
+	public void setTutorialStage(int stage) {
+		tutorialStage = stage;
+	}
+	
+	//Create accept
+	public void createAccept() {
+		accept = new Accept();
+	}
+}
